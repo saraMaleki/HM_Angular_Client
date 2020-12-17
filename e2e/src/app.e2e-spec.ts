cf7c8768c9b9d7149e8effa9c43a02ba305aca22
sara maleki <sarah.maleki83@gmail.com>
2020-12-17 23:16:07 +0330

    minor chnages in texts

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('first-app app is running!');
+    expect(page.getTitleText()).toEqual('hypermapper_angular_client app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'first-app'`, () => {
+  it(`should have as title 'hypermapper_angular_client'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('first-app');
+    expect(app.title).toEqual('hypermapper_angular_client');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('first-app app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('hypermapper_angular_client app is running!');
   });
 });
